799e51033f961d321888c5dcc7833f353d5579d9
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-10 23:23:39 +0600

    upadted

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Banner from "../../Shared/Banner/Banner";
+import About from "../About/About";
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About />
         </div>
     );
 };
